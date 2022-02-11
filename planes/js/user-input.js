7e3e9f5700c501dd01cd6fce1a76ac00d36e5f2b
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2022-02-11 21:24:07 +0000

    plane

--- planes/js/user-input.js
+++ planes/js/user-input.js
@@ -1,34 +1,40 @@
-export { UserInput };
+////export { UserInput };
 
-class UserInput {
-    constructor() {
-        this.upPressed = 10;
-        this.downPressed = false;
-    }
+////class UserInput {
+////    constructor() {
+////        this.upPressed = false;
+////        this.downPressed = false;
+////    }
 
-    eventListener() {
-        document.addEventListener("keydown", this.keyDownHandler, false);
-        document.addEventListener("keyup", this.keyUpHandler, false);
-        document.addEventListener("mousemove", this.mouseMoveHandler, false);
-    }
+////    eventListener() {
+////        document.addEventListener("keydown", this.keyDownHandler, false);
+////        document.addEventListener("keyup", this.keyUpHandler, false);
+////        document.addEventListener("mousemove", this.mouseMoveHandler, false);
+////    }
 
-    keyDownHandler(e) {
-        if (e.key == "Up" || e.key == "ArrowUp") {
-            //this.upPressed = this.upPressed + 10;
-            console.log("111111", this.upPressed);
-        }
-        //else if (e.key == "Down" || e.key == "ArrowDown") {
-        //    this.downPressed += -1;
-        //}
+////    keyDownHandler(e) {
+////        if (e.key == "Up" || e.key == "ArrowUp") {
+////            this.setUpPressed(true);
+////            console.log("111111", this.upPressed); // unique this
+////        }
+////        //else if (e.key == "Down" || e.key == "ArrowDown") {
+////        //    this.downPressed = true;
+////        //}
 
-    }
+////    }
 
-    //keyUpHandler(e) {
-    //    if (e.key == "Up" || e.key == "ArrowUp") {
-    //        this.upPressed += 100;
-    //    }
-    //    else if (e.key == "Down" || e.key == "ArrowDown") {
-    //        this.downPressed += -2;
-    //    }
-    //}
-}
+////    setUpPressed(newVal) {
+////        this.upPressed = newVal;
+////        console.log("upPresseed", this.upPressed)
+////        return this.upPressed;
+////    }
+
+////    keyUpHandler(e) {
+////        //if (e.key == "Up" || e.key == "ArrowUp") {
+////        //    this.upPressed = false;
+////        //}
+////        //else if (e.key == "Down" || e.key == "ArrowDown") {
+////        //    this.downPressed = false;
+////        //}
+////    }
+////}
